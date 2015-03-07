ce4d4158972986a640c9b3fb90e421eb96cbe821
Calle Kabo <calle@kabo.nu>
2015-03-07 21:00:23 +0100

    Added support and preference for mBTC

--- data/ui.js
+++ data/ui.js
@@ -1,26 +1,31 @@
-var btc_input  = document.getElementById("btc_input");
-var usd_value  = document.getElementById("usd_value");
-var usd_wrap   = document.getElementById("usd");
-var rate_value = document.getElementById("rate_value");
-var spinner    = document.getElementById("spinner");
-var timeout    = undefined;
+var btc_input   = document.getElementById("btc_input");
+var usd_value   = document.getElementById("usd_value");
+var usd_wrap    = document.getElementById("usd");
+var rate_value  = document.getElementById("rate_value");
+var spinner     = document.getElementById("spinner");
+var unit_symbol = document.getElementById("unit_symbol");
+var timeout     = undefined;
 
 btc_input.addEventListener('keyup', function onkeyup(event) {
-  var val = btc_input.value;
 	if (timeout != undefined) {
 	 clearTimeout(timeout);
 	}
 	timeout = setTimeout(function() {
 	  timeout = undefined;
-		if (!val.length) {
-			usd_value.textContent = "0.00";
-			return;
-		}
-		self.port.emit("pleaseConvert", val);
-		usd_wrap.style.display = "none";
-		spinner.style.display = "block";
+	  pleaseConvert();
 	}, 300);
 }, false);
+
+function pleaseConvert() {
+  var val = btc_input.value;
+	if (!val.length) {
+		usd_value.textContent = "0.00";
+		return;
+	}
+	self.port.emit("pleaseConvert", val);
+	usd_wrap.style.display = "none";
+	spinner.style.display = "block";
+}
 
 self.port.on("newConversion", function(usd){
   console.info("received newConversion with usd=%s, setting usd_value...", usd);
@@ -31,7 +36,10 @@ self.port.on("newConversion", function(usd){
 self.port.on("newRate", function(rate){
   console.info("received newRate with rate=%s...", rate);
 	rate_value.textContent = rate;
+	pleaseConvert();
 });
-self.port.on("show", function onShow() {
+self.port.on("show", function(unit) {
+  btc_input.setAttribute("placeholder", unit);
   btc_input.focus();
+  unit_symbol.textContent = "mBTC"==unit ? "m฿" : "฿" ;
 });
